58605f496abe21051da5e77a1e7a967022431326
Tiago Gadonski Cordeiro <62782751+TiagoGadonski@users.noreply.github.com>
2024-04-26 19:06:46 -0300

    Update Content.tsx

--- src/app/components/Content.tsx
+++ src/app/components/Content.tsx
@@ -1,29 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Definindo tipos para as props
 interface ContentProps {
-  language: string;  // Definindo que language é uma string
+  language: string; 
 }
 
 const Content: React.FC<ContentProps> = ({ language }) => {
-  // Estado que armazena a lista de IDs de vídeos da playlist atual
   const [playlist, setPlaylist] = useState<string[]>([]);
-  // Estado que armazena o ID do vídeo principal
   const [mainVideoId, setMainVideoId] = useState<string>('');
 
-  // Efeito para atualizar a playlist e o vídeo principal com base no idioma selecionado
-  useEffect(() => {
+   useEffect(() => {
     const playlists: { [key: string]: string[] } = {
       'en-US': ['cTLngWAaIkA', 'CRf_-8yN9es'],
       'pt-BR': ['zdvdBC4aF8w', 'ZcPm9-NPomI'],
       'fr-FR': ['Usc2nYeDumI', '_5FeQfyVLQ8']
     };
     setPlaylist(playlists[language] || []);
-    setMainVideoId(playlists[language][0]);  // Define o primeiro vídeo da lista como o principal
+    setMainVideoId(playlists[language][0]);  
   }, [language]);
 
-  // Função para mudar o vídeo principal quando uma miniatura é clicada
   const handleVideoSelect = (videoId: string) => {
     setMainVideoId(videoId);
   };
