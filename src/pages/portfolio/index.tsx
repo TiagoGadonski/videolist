2ce98787a134f0a30d17878275e29372bbe0d79b
Tiago Gadonski Cordeiro <62782751+TiagoGadonski@users.noreply.github.com>
2024-04-26 19:05:51 -0300

    Update index.tsx

--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -30,17 +30,13 @@ const projects = [
 
 const PortfolioPage: React.FC = () => {
   const [language, setLanguageState] = useState('en-US');
-  
-  // Função para atualizar o estado da linguagem e logar mudanças
-  const setLanguage = (lang: string) => {
-    console.log("Language set to:", lang);
-    setLanguageState(lang);  // Atualiza o estado
+    const setLanguage = (lang: string) => {
+    setLanguageState(lang);  
   };
 
   useEffect(() => {
-    // Este código só será executado no cliente
     const initialLanguage = navigator.language.includes('pt') ? 'pt-BR' : navigator.language.includes('fr') ? 'fr-FR' : 'en-US';
-    setLanguage(initialLanguage);  // Define o idioma inicial baseado no navegador
+    setLanguage(initialLanguage);
   }, []);
   return (
     <div>
