fd7e629914747bcc27197da4e364c388571e1c8a
Tiago Gadonski Cordeiro <62782751+TiagoGadonski@users.noreply.github.com>
2024-04-26 19:05:03 -0300

    Update page.tsx

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -5,19 +5,18 @@ import Menu from '../app/components/Menu';
 import Content from '../app/components/Content';
 
 const App = () => {
-  // Inicializa o estado da linguagem com um valor padrão
+
   const [language, setLanguageState] = useState('en-US');
   
-  // Função para atualizar o estado da linguagem e logar mudanças
+  
   const setLanguage = (lang: string) => {
-    console.log("Language set to:", lang);
-    setLanguageState(lang);  // Atualiza o estado
+     setLanguageState(lang);  
   };
 
   useEffect(() => {
-    // Este código só será executado no cliente
+    
     const initialLanguage = navigator.language.includes('pt') ? 'pt-BR' : navigator.language.includes('fr') ? 'fr-FR' : 'en-US';
-    setLanguage(initialLanguage);  // Define o idioma inicial baseado no navegador
+    setLanguage(initialLanguage); 
   }, []);
 
   return (
